29ba531aadbf72b683aa1173160081b7a6b2c07c
a1000rhan <1000rhan@gmail.com>
2021-12-22 13:55:31 +0300

    calc3

--- calculator.js
+++ calculator.js
@@ -2,12 +2,18 @@ const prompt = require("prompt-sync")(); // Please don't touch me :)
 
 // The prompt method allows us to take an input from the user.
 // Run the code to see how to use prompt.
+
+
 let num1 = prompt("What's 1st number ");
 let num2 = prompt("What's 2nd number ");
 let op = prompt("What's op ");
 let res=0;
+if(isNaN(num1) || isNaN(num2) || op == null ){
+    console.log("try again");
 
-num1=parseInt(num1);
+
+}else{
+    num1=parseInt(num1);
 num2=parseInt(num2);
 if(op == "+"){
     
@@ -28,5 +34,7 @@ console.log(`result =  ${res}`);
     res=num1*num2;
     console.log(`result =  ${res}`);
 
+}
+    
 }
 
